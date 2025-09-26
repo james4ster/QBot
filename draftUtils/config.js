ffcf6c91b2b10cec5d43ea6f71d3c406ead3115e
Jamie <51055904+james4ster@users.noreply.github.com>
2025-09-26 14:36:23 +0000

    Remove draftUtils/ from repo, keep local

--- draftUtils/config.js
+++
@@ -1,5 +0,0 @@
-// for DRAFT_CHANNEL_ID, using TickleTesting for testing
-
-export const SPREADSHEET_ID = process.env.COPY_OF_Q_SPREADSHEET_ID;
-export const DRAFT_SHEET_NAME = 'Draft Central - 98';
-export const DISCORD_CHANNEL_ID = process.env.DRAFT_CHANNEL_ID;
